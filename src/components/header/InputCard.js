70c0b7b42b273d03ff2e2c5a2099695baf622547
Manas Khanna <manas.khanna@avisoft.io>
2024-03-04 17:47:26 +0530

    this is card component

---
+++ src/components/header/InputCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function InputCard() {
+    return (
+        <div>
+            <div className=''>
+                <input className='transition-all border rounded-lg focus:outline-blue-300  text-center w-[150px] h-[35px]' type="text" name="" id="" placeholder='Enter a code' />
+            </div>
+            <div className='border-r h-3 mr-[75px]'></div>
+            <div>
+                <button class="bg-purple-400 text-lg text-white w-[170px] py-2 px-4 rounded">Get Access</button>
+            </div>
+        </div>
+    )
+}
+
+export default InputCard
