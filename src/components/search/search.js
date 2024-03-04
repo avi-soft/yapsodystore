0e11d6a0d5174b2f260c64c9c31db1077b72dc95
Dishant-Balotra-Avisoft <dishant.balotra@avisoft.io>
2024-03-04 23:36:05 +0530

    Footer and Search

---
+++ src/components/search/search.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+
+import searchImg from "../../../public/magnifying-glass-solid.svg";
+import styles from "./search.module.css";
+
+const Search = () => {
+  return (
+    <div className=" mt-16 mb-6 w-fit">
+      <Image src={searchImg} alt="search" className={styles.icon} />
+      <input
+        type="search"
+        name="search"
+        id="search"
+        className={`${styles.search} md:w-[250px] w-fit`}
+        placeholder="Search Events"
+      />
+    </div>
+  );
+};
+
+export default Search;
