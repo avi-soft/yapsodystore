945c272fd84cd3153213be0baa47fcb7a4ea9819
Arun <arun.kumar@avisoft.io>
2024-03-05 18:26:58 +0530

    add calendar component

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
+import { Calendar } from "@/components/calendar/calendar";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
+        <Calendar
+          highlighted={[new Date(2024, 2, 10), new Date(2024, 2, 14)]}
+          activeColorCode={"blue"}
+        />
       </div>
     </main>
   );
