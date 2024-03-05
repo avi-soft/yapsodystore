5468da56db37a36b742cdd752234f406c91c4712
Arun-Kumar-Avisoft <160815041+Arun-Kumar-Avisoft@users.noreply.github.com>
2024-03-05 18:51:44 +0530

    29026 calander layout (#12)
    * add calendar component
    
    * add footer

--- src/helpers/api-utils.js
+++ src/helpers/api-utils.js
@@ -1,7 +1,33 @@
-const BASE_URL = process.env.YAPSODY_API_BASE_URL;
+import request from "./api-urls";
+
+const BaseUrl =
+  process.env.ENV === "dev"
+    ? "https://stage-api.yapsody.com/"
+    : "https://api.yapsody.com/";
+
+const headerData = {
+  headers: {
+    "venue-code": "myblog",
+  },
+};
+
+export async function getData(...config) {
+  try {
+    const [url, headerData = {}] = config;
+    const response = await fetch(url, headerData);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Rethrow the error to be caught by the calling function
+  }
+}
 
 export async function getTheme() {
-  const data = await fetch(BASE_URL + "");
-  Jsondata = await data.json();
-  return Jsondata;
+  const response = await getData(BaseUrl + request.getVenueDetails, headerData);
+  return response;
 }
