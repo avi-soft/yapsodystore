8ea2041966ef0d72d14b78c07f0854be83dc0ce3
Manas Khanna <manas.khanna@avisoft.io>
2024-03-05 11:15:36 +0530

    card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-import Card from "@/components/header/Card";
+import Card from "@/components/Card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+      <div className="w-full items-center justify-between font-mono text-sm">
         <Card/>
-        
+        <Card/>
+        <Card isCodeAvailable={false}/>
+
       </div>
     </main>
   );
