899963637f8716d10bb7604d71984e89b65f9f7c
Dishant-Balotra-Avisoft <160771803+Dishant-Balotra-Avisoft@users.noreply.github.com>
2024-03-05 18:05:38 +0530

    28952 replicate footer and search (#7)
    * test
    
    * Footer and Search

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Search from "@/components/search/search";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
+        <Search />
       </div>
     </main>
   );
