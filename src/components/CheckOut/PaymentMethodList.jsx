2d6164d9272060c135cfd9397e436dac0764a6e8
Deep <deepsinghyadav933@gmail.com>
2024-03-06 10:37:02 +0530

    too many changes

--- src/components/CheckOut/PaymentMethodList.jsx
+++ src/components/CheckOut/PaymentMethodList.jsx
@@ -44,19 +44,19 @@ const PaymentMethodList = ({ Paymentmethods }) => {
                           className="before:content[''] peer relative h-6 w-6 cursor-pointer appearance-none rounded-md border border-[var(--highlightColor)] transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-12 before:w-12 before:-translate-x-2/4 before:-translate-y-2/4 before:rounded-full before:bg-[var(--highlightColor)] before:opacity-0 before:transition-opacity checked:border-[var(--highlightColor)] checked:bg-[var(--highlightColor)] checked:before:bg-[var(--highlightColor)] hover:before:opacity-10 focus:ring-transparent checked:hover:bg-[var(--highlightColor)]"
                           id="checkcondition"
                         />
-                        <span class="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
+                        <span className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-3.5 w-3.5"
                             viewBox="0 0 20 20"
                             fill="currentColor"
                             stroke="currentColor"
-                            stroke-width="1"
+                            strokeWidth="1"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </span>
