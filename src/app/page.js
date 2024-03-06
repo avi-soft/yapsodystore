534ff2df63066a381eaa375444a3829b07bf8af2
Arun <arun.kumar@avisoft.io>
2024-03-06 17:38:50 +0530

    fixed the page issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
 import Calendar from "@/components/calendar/calendar";
 import Search from "@/components/search/search";
-import Card from "@/components/Card";
-import EventList from "@/components/events/EventList";
-import BoxContainer from "@/components/single-event-page/BoxContainer";
-import SideBarEventPage from "@/components/single-event-page/SideBarEventPage";
-import MainContent from "@/components/single-event-page/MainContent";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-0 pr-[120px] pb-[30px] pl-[120px] m-auto" >
+    <main className="flex min-h-screen flex-col items-center justify-between pt-0 pr-[120px] pb-[30px] pl-[120px] m-auto">
       <div>
         <Search />
         <Calendar
@@ -17,12 +12,7 @@ export default function Home() {
         />
         {/* <Card />
         <Card isCodeAvailable={false} /> */}
-
       </div>
-      <div className="mt-[20px] w-[100%]">
-       {/* <EventList/> */}
-       <BoxContainer children1={<SideBarEventPage />} children2={<MainContent />} color="lightblue" />
-       </div>
     </main>
   );
 }
