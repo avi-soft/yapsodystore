3c25331f6f54284d34c788ebbe85912de4465c5a
Deep <deepsinghyadav933@gmail.com>
2024-03-07 11:19:56 +0530

    too many changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import BoxContainer from "@/components/single-event-page/BoxContainer";
 import SideBarEventPage from "@/components/single-event-page/SideBarEventPage";
 import MainContent from "@/components/single-event-page/MainContent";
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pt-0 pr-[120px] pb-[30px] pl-[120px] m-auto" >
@@ -22,6 +23,7 @@ export default function Home() {
       <div className="mt-[20px] w-[100%]">
        {/* <EventList/> */}
        <BoxContainer children1={<SideBarEventPage />} children2={<MainContent />} color="lightblue" />
+
        </div>
     </main>
   );
