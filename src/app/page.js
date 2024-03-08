1c6508b323a35b7d5eb3dc712b5e8a3b39b2d86c
Megha-Gupta-Avisoft <megha.gupta@avisoft.io>
2024-03-08 10:47:14 +0530

    modified title header of home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 import Calendar from "@/components/calendar/Calendar";
 import Search from "@/components/search/search";
 import SocialMedia from "@/components/social-media/SocialMedia";
 import MainContainer from "@/components/main-container/MainContainer";
 import MainPageEventList from "@/components/events/MainPageEeventList";
 import SupportContact from "@/components/support-contact/SupportContact";
+import Title from "@/components/homepage-header/Title";
 
 export default function Home() {
   const themeColor = "red";
@@ -16,9 +15,7 @@ export default function Home() {
       }
     >
       <div className="w-full">
-        <div className="p-8 flex justify-center">
-          <Image src="/store-logo.png" alt="image" width={30} height={30} />
-        </div>
+     <Title logoImage="https://s3.amazonaws.com/ft-images/venue_logo/92ba6bf256c6fe122cc882700fe996fa825740a6.jpg" />
         <SocialMedia position="center" />
         <SupportContact position="center" />
         {/* <Card />
