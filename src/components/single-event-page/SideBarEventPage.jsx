f47d82d8664bf6169e307dab376d85c3e153273e
Deep <deepsinghyadav933@gmail.com>
2024-03-11 02:08:26 +0530

    too many changes

--- src/components/single-event-page/SideBarEventPage.jsx
+++ src/components/single-event-page/SideBarEventPage.jsx
@@ -21,7 +21,7 @@ const SideBarEventPage = ({ color }) => {
           height={100}
           className="mb-[30px] h-[180px] min-w-[100%] rounded border-4 border-[#ffffff] bg-[#cccccc] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.3)]"
         ></Image>
-        <p className="absolute mt-[-115px] max-w-[100%] rotate-[-30deg] whitespace-nowrap pl-[95px] text-[rgba(51,51,51,0.5)] max-xl:ml-[100px] max-xl:mt-[-130px] max-xl:rotate-[]">
+        <p className="absolute mt-[-85px] max-w-[100%] rotate-[-30deg] whitespace-nowrap pl-[95px] text-[rgba(51,51,51,0.5)] max-xl:ml-[100px] max-xl:mt-[-130px] max-xl:rotate-[]">
           {event.name}
         </p>
       </div>
