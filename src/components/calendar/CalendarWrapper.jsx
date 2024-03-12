81c91c8e8a1a85c8fb54e86dbacd9091c8e9c0fc
Arnav-khajuria-avisoft <160816425+Arnav-khajuria-avisoft@users.noreply.github.com>
2024-03-12 17:23:11 +0530

    29145 calender responsive (#48)
    * Fixes to header & footer
    
    * Changes to lang dropdown
    
    * dropdown elements color fix
    
    * Changes to calender

--- src/components/calendar/CalendarWrapper.jsx
+++ src/components/calendar/CalendarWrapper.jsx
@@ -1,16 +1,64 @@
+"use client"
+
+import React, { useState } from 'react';
+
+import { FaCalendarAlt } from "react-icons/fa";
+import { RxCrossCircled } from "react-icons/rx";
+
 export default function CalendarWrapper({ children, performancesCount }) {
+
   return (
-    <div className="border-b mb-2 w-[100%]">
-      <div className="flex">
-        <div className="flex gap-2 pr-2 border-r slate-300">
-          <span>Upcoming Events ({performancesCount})</span>
+    <div>
+      <div className="sm:hidden flex  border-b mb-2 gap-2 w-[80vw] ">
+        <div className="flex ">
+          <div className="flex gap-2 pr-2 border-r  slate-300 ">
+            <span>Upcoming Events ({performancesCount})</span>
+          </div>
         </div>
-        <div className="ml-2">
-          <div className="w-full">
+        <div>
+          <button onClick={() => document.getElementById('my_modal_5').showModal()}><div tabIndex={0} role="button">
+            <FaCalendarAlt className="size-6 cursor-pointer" />
+          </div></button>
+        </div>
+
+
+        <dialog id="my_modal_5" className="modal modal-center sm:modal-center ">
+          <div className="">
             <div className="dropdown">{children}</div>
+            <div className="flex justify-center mt-5">
+              <form method="dialog">
+                <div className="flex flex-row items-center ">
+                  <RxCrossCircled className="cursor-pointer text-white" />
+                  <button className=" ml-2 bg-transparent text-white items-center">CLOSE</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+
+      <div className="border-b mb-2 w-[86vw]  sm:block hidden  ">
+        <div className="flex ">
+          <div className="flex gap-2 pr-2 border-r slate-300 ">
+            <span>Upcoming Events ({performancesCount})</span>
+          </div>
+          <div className="ml-2">
+            <div className="w-full">
+              <div className="dropdown">
+                <div tabIndex={0} role="button">
+                  <FaCalendarAlt className="size-6 cursor-pointer" />
+                </div>
+                <div
+                  tabIndex={0}
+                  className="dropdown-content z-[100]"
+                >
+                  {children}</div></div>
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
+
