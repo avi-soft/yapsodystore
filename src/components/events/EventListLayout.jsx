460b4de60665cd56800521178bc544f5aa4e9fc5
Arun-Kumar-Avisoft <160815041+Arun-Kumar-Avisoft@users.noreply.github.com>
2024-03-12 19:34:00 +0530

    add api (#52)

--- src/components/events/EventListLayout.jsx
+++ src/components/events/EventListLayout.jsx
@@ -1,9 +1,17 @@
+"use client";
 import Button from "@/ui/Button";
 import EventDetails from "./EventDetails";
 import EventImage from "./EventImage";
 
 export default function EventListLayout({
-  color,
+  eventId,
+  showImage,
+  locationId,
+  locationType,
+  buttonLinkBoxBorderColor,
+  boxBackgroundColor,
+  headingColor,
+  textColor,
   eventName,
   location,
   startDate,
@@ -11,20 +19,26 @@ export default function EventListLayout({
   performances,
 }) {
   return (
-    <div className="mb-[20px] w-[100%] flex justify-between rounded border-[1px] border-[#a593e0] bg-[#ffffff]   max-md:flex-col max-md:gap-3">
+    <div
+      style={{
+        borderColor: buttonLinkBoxBorderColor,
+        backgroundColor: boxBackgroundColor,
+      }}
+      className="mb-[20px] w-[100%] flex justify-between rounded border-[1px]    max-md:flex-col max-md:gap-3"
+    >
       <div className="relative min-h-[1px] justify-center max-md:w-[100%]">
         <div className="h-[180px] flex-col  text-center">
-          <EventImage
-            src={
-              "https://yaptestaddon4.staging.yapsody.com/images/new_ci/styles/images/default-img.jpg"
-            }
-          />
+          <EventImage src={showImage} />
           <p className="absolute mt-[-80px] max-w-[100%] rotate-[-30deg] whitespace-nowrap pl-[68px] text-[15px] text-[rgba(51,51,51,0.5)]">
-            {eventName}
+            {!showImage && eventName}
           </p>
         </div>
       </div>
       <EventDetails
+        locationId={locationId}
+        locationType={locationType}
+        headingColor={headingColor}
+        textColor={textColor}
         eventName={eventName}
         location={location}
         startDate={startDate}
@@ -35,9 +49,12 @@ export default function EventListLayout({
       <div className="relative mt-[72px] min-h-[1px] w-[33.33%] px-[25px] text-right max-md:w-[100%] max-md:mt-[60px]">
         <div className="w-[70%] rounded  py-[6px] text-center">
           <Button
-            width={"100%"}
-            color={color}
-            to={"/event/index/26809/new-event"}
+            width={"60%"}
+            color={buttonLinkBoxBorderColor}
+            to={`/event/index/${eventId}/${eventName
+              .replace(/[^\w\s]/g, "")
+              .replace(/\s+/g, "-")
+              .replace(/-+$/, "")}`}
           >
             More Info
           </Button>
