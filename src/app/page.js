341eea60def325d4c324154cce3784c4d483d23c
Manas Khanna <manas.khanna@avisoft.io>
2024-03-12 13:28:04 +0530

    Update Processing Ticket

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import MainPageEventList from "@/components/events/MainPageEeventList";
 import SupportContact from "@/components/support-contact/SupportContact";
 import Title from "@/components/homepage-header/Title";
 
+
 export default function Home() {
   const themeColor = "red";
   return (
--- src/components/processing-ticket/ProcessingTicket.jsx
+++ src/components/processing-ticket/ProcessingTicket.jsx
@@ -1,30 +1,20 @@
 "use client"
 import React from 'react'
 
-// npm innstall ldrs for loading bar
-import { lineWobble } from 'ldrs';
-
-if (typeof window !== 'undefined') {
-    // Browser-specific code here
-    lineWobble.register();
-}
+import "./processingTicket.css"
 
 
 function ProcessingTicket() {
     return (
         <div>
             <div className='mr-auto ml-auto flex flex-col justify-center items-center'>
-                <h1 className='text-2xl mb-4'>Processing your Ticket...</h1>
+                <h1 className='text-2xl mb-6'>Processing your Ticket...</h1>
+
+                <div className="bg-[#53ac1cab] h-[12px] p-[1px] w-[15%] mr-auto ml-auto rounded-sm">
+                    <div class="fill c"></div>
+                </div>
 
 
-                <l-line-wobble
-                    size="80"
-                    stroke="5"
-                    bg-opacity="0.1"
-                    speed="1.75"
-                    color="black"
-                ></l-line-wobble>
-                
                 <div className='mt-3 text-slate-500'>
                     This can take up to 3 minutes. DO NOT CLOSE the browser press the back button.
                 </div>
