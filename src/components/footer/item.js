3519f6f948179bfe4df73f42ebf913b57386a024
Arun-Kumar-Avisoft <160815041+Arun-Kumar-Avisoft@users.noreply.github.com>
2024-03-13 10:49:56 +0530

    Revert "29150 footer UI issue (#51)"
    This reverts commit c3aa60ae77241b5c75ecdebbffac4ae417df7731.

--- src/components/footer/item.js
+++ src/components/footer/item.js
@@ -5,13 +5,13 @@ import styles from "./footer.module.css";
 const Item = ({ text, link, hide }) => {
   return hide ? (
     <li className={`cursor-pointer flex ${styles.hide}`}>
-      <span className="w-1.5 mr-3 h-1.5 rounded-full bg-[#d0d8e5] mt-1 inline-block self-center"></span>
+      <span className="w-1.5 mr-4 h-1.5 rounded-full bg-slate-400 inline-block self-center"></span>
       <Link href={link}> {text}</Link>
     </li>
   ) : (
     <li className={`cursor-pointer flex `}>
       {text !== "Support" && (
-        <span className="w-1.5 mr-3 h-1.5 rounded-full bg-[#d0d8e5] mt-1 inline-block self-center"></span>
+        <span className="w-1.5 mr-4 h-1.5 rounded-full bg-slate-400 inline-block self-center"></span>
       )}
 
       <Link href={link}> {text}</Link>
