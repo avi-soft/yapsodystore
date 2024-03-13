5a8b474c6363bdc72e98b7bdd00766d23b5e74b6
Megha-Gupta-Avisoft <megha.gupta@avisoft.io>
2024-03-13 12:08:40 +0530

    support box color fixed

--- src/components/support-contact/SupportContact.jsx
+++ src/components/support-contact/SupportContact.jsx
@@ -1,11 +1,11 @@
 import Support from "../support/Support";
 import ContactPresenter from "../contact/ContactPresenter";
 import PresenterFaq from "../presentor-faq/PresenterFaq";
-export default function SupportContact({ position, iconColor = "black" }) {
+export default function SupportContact({ position, iconColor = "black",textColor }) {
   return (
     <div className={`flex gap-x-4 gap-y-1 justify-${position}  p-5 flex-wrap`}>
       <span className="max-sm:mt-[10px] ">
-        <Support size="size-6" color={iconColor} />
+        <Support size="size-6" color={iconColor} textColor={textColor} />
       </span>
       <span className="max-sm:mt-[10px]">
         <ContactPresenter size="size-6" color={iconColor} />
