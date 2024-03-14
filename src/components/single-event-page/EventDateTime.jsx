270d91b42bf618f4fea150821388d20e66815403
Megha-Gupta-Avisoft <megha.gupta@avisoft.io>
2024-03-14 16:45:31 +0530

    colors for performance box fixed

--- src/components/single-event-page/EventDateTime.jsx
+++ src/components/single-event-page/EventDateTime.jsx
@@ -1,10 +1,16 @@
-export default function EventDateTime({ date, time }) {
+export default function EventDateTime({ date, time, headingColor, textColor }) {
   return (
     <div className="relative min-h-[1px] w-[33.33%] border-r pl-[10px] pr-[10px] max-md:w-[100%] max-md:border-r-0 ">
-      <h4 className="mb-[8px] mt-[17px] text-center text-[22px] font-[500] text-[#21262]">
+      <h4
+        style={{ color: headingColor }}
+        className="mb-[8px] mt-[17px] text-center text-[22px] font-[500] text-[#21262]"
+      >
         {date}
       </h4>
-      <p className="mb-[15px] text-center text-[16px] font-[500] text-[var(--text-primary)]">
+      <p
+        style={{ color: textColor }}
+        className="mb-[15px] text-center text-[16px] font-[500] text-[var(--text-primary)]"
+      >
         {time}
       </p>
     </div>
