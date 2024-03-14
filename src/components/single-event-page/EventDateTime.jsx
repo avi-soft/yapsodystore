ca9077a6dd7f0eb03bd54b507257d2f385849d06
Megha-Gupta-Avisoft <megha.gupta@avisoft.io>
2024-03-14 17:29:37 +0530

    colors updated

--- src/components/single-event-page/EventDateTime.jsx
+++ src/components/single-event-page/EventDateTime.jsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 export default function EventDateTime({ date, time, headingColor, textColor }) {
   return (
     <div className="relative min-h-[1px] w-[33.33%] border-r pl-[10px] pr-[10px] max-md:w-[100%] max-md:border-r-0 ">
@@ -5,13 +6,13 @@ export default function EventDateTime({ date, time, headingColor, textColor }) {
         style={{ color: headingColor }}
         className="mb-[8px] mt-[17px] text-center text-[22px] font-[500] text-[#21262]"
       >
-        {date}
+        {moment(date).format("MMM D, YYYY")}
       </h4>
       <p
         style={{ color: textColor }}
         className="mb-[15px] text-center text-[16px] font-[500] text-[var(--text-primary)]"
       >
-        {time}
+        {moment(date).format("LT")}
       </p>
     </div>
   );
