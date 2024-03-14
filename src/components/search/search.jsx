af991615bdf0426f71263f5b4efdadf5b2cdcbe9
Deep <deepsinghyadav933@gmail.com>
2024-03-14 10:48:23 +0530

    commit

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
 import styles from "./search.module.css";
 
-const Search = ({ color, textColor, buttonLinkBoxBorderColor }) => {
+const Search = ({ color, textColor, buttonLinkBoxBorderColor, onSearchChange }) => {
   const inputStyle = {
     borderColor: color,
     backgroundColor: color,
     color: textColor,
     borderColor: buttonLinkBoxBorderColor,
   };
+  // const 
+  const handleChange = (e) => {
+    onSearchChange(e.target.value);
+  }
   return (
     <div className="mb-6 max-sm:w-[100%]">
       <IoSearchSharp color={textColor} className={styles.icon} />
