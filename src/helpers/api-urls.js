06627cf1ac43c8c07058c3c8d17894846ca5027f
Arun <arun.kumar@avisoft.io>
2024-03-14 10:06:06 +0530

    add api data

--- src/helpers/api-urls.js
+++ src/helpers/api-urls.js
@@ -4,5 +4,6 @@ const request = {
   language: "languages",
   languageTranslation: "languages/translations?lang=hi",
   singleEventPerformances: (eventId) => `online/events/${eventId}/performances`,
+  singleEvent: (eventId) => `online/events/${eventId}`,
 };
 export default request;
