b314b0a0f649e6b29bcd6a988b87215ea308b9f2
Arnav-Khajuria-avisoft <arnav.khajuria@avisoft.io>
2024-03-18 15:34:56 +0530

    Minor Bug Fixed

--- src/components/event-book-page/ticketselector/BookingHeader.jsx
+++ src/components/event-book-page/ticketselector/BookingHeader.jsx
@@ -37,14 +37,12 @@ const BookingHeader = async ({ iconColor }) => {
         <div className="drawer drawer-end">
   <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content">
-    {/* Page content here */}
-    
     <label htmlFor="my-drawer-4" className="drawer-button "><BsInfoCircleFill size={32}/></label>
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
     <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
+      {/* Sidebar content here */}      
       
     </ul>
   </div>
