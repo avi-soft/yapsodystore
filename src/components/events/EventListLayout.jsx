32b4afe659423ac7a1f5431004a541c81fb1e366
Dishant-Balotra-Avisoft <dishant.balotra@avisoft.io>
2024-03-18 11:39:58 +0530

    bug fixes

--- src/components/events/EventListLayout.jsx
+++ src/components/events/EventListLayout.jsx
@@ -36,7 +36,7 @@ export default function EventListLayout({
           .replace(/-+$/, "")}`}
         className="relative min-h-[100%] justify-center max-md:w-[100%] max-md:h-[180px] px-[4px] py-[4px]"
       >
-        <div className="lg:w-[190px] lg:h-[142.5px] max-h-[154px] w-[95%] relative overflow-hidden object-fill">
+        <div className="lg:w-[190px] lg:h-[142.5px] md:w-[170px] md:h-[127.5px] max-h-[154px] w-[95%] relative overflow-hidden object-fill">
           <EventImage src={showImage} />
           <p className=" text-center w-[90%] absolute top-0 bottom-0 left-0 right-0 h-fit m-auto rotate-[-30deg] whitespace-nowrap text-[15px] text-[rgba(51,51,51,0.5)] px-[12px] overflow-hidden text-ellipsis max-md:rotate-[-10deg] max-sm:rotate-[-20deg]">
             {!showImage && eventName}
--- src/components/presentor-faq/DisplayFaq.jsx
+++ src/components/presentor-faq/DisplayFaq.jsx
@@ -1,31 +1,48 @@
-'use client'
-import React from 'react'
-import { useState } from 'react';
-import './style.css'
+"use client";
+import React from "react";
+import { useState } from "react";
+import "./style.css";
 
-const DisplayFq = ({faq}) => {
+const DisplayFq = ({ faq }) => {
   const [isAnswerVisible, setIsAnswerVisible] = useState(false);
 
   const toggleAnswerVisibility = () => {
     setIsAnswerVisible(!isAnswerVisible);
   };
   return (
-    <div className='present-faq'>
-      <div className='inside-faq'>
-        <div style={{fontWeight:'600', color:'rgba(1, 22, 56, 0.7)' , cursor:'pointer', flex:80, fontSize:'1rem'}}  onClick={toggleAnswerVisibility}>
+    <div className="present-faq">
+      <div className="inside-faq">
+        <div
+          style={{
+            fontWeight: "600",
+            color: "rgba(1, 22, 56, 0.7)",
+            cursor: "pointer",
+            flex: 80,
+            fontSize: "1rem",
+          }}
+          onClick={toggleAnswerVisibility}
+        >
           {faq.question}
         </div>
-        <div style={{flex:20 , display:'flex', justifyContent:'center', color:'grey'}}>
-          <spa>{isAnswerVisible ? <span >&#8896;</span> : <span >&#8897;</span>}</spa>
+        <div
+          style={{
+            flex: 20,
+            display: "flex",
+            justifyContent: "center",
+            color: "grey",
+          }}
+        >
+          <span>
+            {isAnswerVisible ? <span>&#8896;</span> : <span>&#8897;</span>}
+          </span>
         </div>
       </div>
-        
+
       {isAnswerVisible && (
-      <div style={{color:'#343131' , margin:'9px 0px '}}>{faq.answer}</div>
+        <div style={{ color: "#343131", margin: "9px 0px " }}>{faq.answer}</div>
       )}
     </div>
-  )
+  );
+};
 
-}
-
-export default DisplayFq
+export default DisplayFq;
