b451da20f3c42d16b4dc2cb5cefb470fa0697ad8
Arun <arun.kumar@avisoft.io>
2024-03-18 12:28:07 +0530

    fix build issue

--- src/app/event/book/[eventid]/[dateid]/page.js
+++ src/app/event/book/[eventid]/[dateid]/page.js
@@ -1,10 +1,10 @@
-import EventPage from "@/components/event-book-page-left-part/page";
+import ShowEventLeft from "@/components/event-book-leftpart/ShowEventLeft";
 export default function SeatBookingPage({ params }) {
   const { eventid, dateid } = params;
   return (
     <div className="pt-24 pb-8">
       This is booking page
-      <EventPage />
+      <ShowEventLeft />
     </div>
   );
 }
