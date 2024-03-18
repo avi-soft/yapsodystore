35fd09466a93407eeb7f666da4db84595408cc7f
Dishant-Balotra-Avisoft <dishant.balotra@avisoft.io>
2024-03-18 13:52:49 +0530

    bug fixes

--- src/components/main-container/MainContainer.jsx
+++ src/components/main-container/MainContainer.jsx
@@ -4,13 +4,13 @@ export default function MainContainer({
   storeBackground,
   isHome = true,
 }) {
+  let px = isHome ? "lg:px-[120px]" : "lg:px-[30px]";
   return (
     <main className="pt-24 pb-8">
       <div
         style={{ backgroundColor: storeBackground }}
-        className={`flex min-h-screen flex-col items-center font-lato justify-between pb-[15px] m-auto w-[97.5%] md:w-[90%] lg:px-[${
-          isHome ? "100px" : "30px"
-        }]  bg-300 bg-opacity-90 rounded-md opacity-[0.95]`}
+        className={`flex ${px}  min-h-screen flex-col items-center font-lato justify-between pb-[15px] m-auto w-[97.5%] md:w-[90%] max-w-[1300px]
+          bg-300 bg-opacity-90 rounded-md opacity-[0.95]`}
       >
         {children}
       </div>
