40cda50aabb9479f8294313ca6dbde8018803e96
Arun-Kumar-Avisoft <160815041+Arun-Kumar-Avisoft@users.noreply.github.com>
2024-03-20 17:22:10 +0530

    29244 click functionality video image (#140)
    * click event fixed
    
    * icon fixed
    
    * sizing fixed
    
    * condition fixed
    
    ---------
    
    Co-authored-by: Megha-Gupta-Avisoft <megha.gupta@avisoft.io>

--- src/components/single-event-page/EventDateTime.jsx
+++ src/components/single-event-page/EventDateTime.jsx
@@ -1,5 +1,5 @@
 import moment from "moment";
-export default function EventDateTime({ date, time, headingColor, textColor }) {
+export default function EventDateTime({ date, headingColor, textColor }) {
   return (
     <div className="relative min-h-[1px] w-[33.33%] border-r border-[#00000012] pl-[10px] pr-[10px] max-md:w-[100%] max-md:border-r-0 ">
       <h4
