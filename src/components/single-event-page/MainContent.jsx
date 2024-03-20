39e139b7390f20db38543d26c94bf49c770ddcba
Arnav-khajuria-avisoft <160816425+Arnav-khajuria-avisoft@users.noreply.github.com>
2024-03-20 18:48:07 +0530

    29153 social icon issues (#133)
    * Booking page Integration
    
    * Conflicts Resolved
    
    * fixed social media issue
    
    * Font changes
    
    * social icon size fixed
    
    * Main content changes
    
    * social media icons changed
    
    ---------
    
    Co-authored-by: Arun <arun.kumar@avisoft.io>
    Co-authored-by: Arun-Kumar-Avisoft <160815041+Arun-Kumar-Avisoft@users.noreply.github.com>

--- src/components/single-event-page/MainContent.jsx
+++ src/components/single-event-page/MainContent.jsx
@@ -54,9 +54,11 @@ const MainContent = ({
         >
           {event_title1}
         </h1>
+       
         <h2 style={headingStyle} className=" text-[18px] font-normal  ">
           {event_title2}
         </h2>
+        
         <h3 style={headingStyle} className=" text-[18px] font-normal">
           {event_title3}
         </h3>
