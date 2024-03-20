721ee37142a7e822c49875b624a6cee09e78d64d
Megha-Gupta-Avisoft <megha.gupta@avisoft.io>
2024-03-20 13:29:52 +0530

    ticket book api page

---
+++ src/components/event-book-page/eventTypes/GeneralEvent.jsx
@@ -0,0 +1,36 @@
+import styles from "@/components/event-book-page/ticketcart/Card.module.css";
+import TicketDropdown from "../ticketcart/Ticketdropdown";
+export default function GeneralEvent({
+  selectedTickets,
+  handleTicketChange,
+  eventSeatData,
+  sectionData,
+  pricingData,
+  handleRemoveTicket,
+}) {
+  return (
+    <div
+      className={`${styles.event} select-ticket lg:w-[50vw] md:w-[38vw]  max-lg:w-[50vw] xl:w-[61vw] max-sm:w-[85vw] md:pr-[100px]`}
+    >
+      <div className="part">
+        <div className="upper-part">Adult </div>
+      </div>
+      <div className="part">
+        <div className="mid-part">
+          <div className="dollar">$50.00</div>
+        </div>
+      </div>
+      <div className=" flex ">
+        <div className="lower-part flex flex-row items-center w-full md:w-auto">
+          <label htmlFor="quantity" className="mr-2">
+            Quantity
+          </label>
+          <TicketDropdown
+            value={selectedTickets}
+            onChange={handleTicketChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/event-book-page/ticketselector/TicketSelector.jsx
+++ src/components/event-book-page/ticketselector/TicketSelector.jsx
@@ -4,63 +4,61 @@ import React from "react";
 import Script from "next/script";
 
 import "./style.css";
-import styles from "@/components/event-book-page/ticketcart/Card.module.css";
-import TicketDropdown from "../ticketcart/Ticketdropdown";
+import TicketSelectorHeading from "./TicketSelectorHeading";
+import GeneralEvent from "../eventTypes/GeneralEvent";
+import SeatsIoEvent from "../eventTypes/SeatsIOEvent";
+import OldSeatingEvent from "../eventTypes/OldSeatingEvent";
 
 
-const TicketSelector = ({ selectedTickets, handleTicketChange }) => {
+const TicketSelector = ({
+  selectedTickets,
+  handleTicketChange,
+  eventSeatData,
+  sectionData,
+  pricingData,
+  handleRemoveTicket
+}) => {
 
-
+  const { event_seating_type, is_seats_io } = eventSeatData;
   return (
     <div>
       <Script
         src="https://kit.fontawesome.com/8d4b434c6d.js"
         crossorigin="anonymous"
       />
-       {/*Event Details */}
-        <div className="getticket-main md:w-[70vw] xl:w-[80vw]  ">
-          <div className="mainbox-event md:block hidden border-solid border-b-[1px] py-2 pb-6  ">
+      <TicketSelectorHeading eventSeatData={eventSeatData} />
 
-
-            <div className="divide-main ">
-
-              <div className="box mr-[20px]">
-                <div className="diagonal">MULTI Events</div>
-              </div>
-              <div className="event-name">MULTI Events</div>
-            </div>
-            <div className="get-time pt-4 ">
-              <div className="mr-4 font-[400] text-base pb-2 ">
-                <i className="fa-solid fa-laptop"></i>
-                <span className="ml-3">Asia/Calcutta</span>
-              </div>
-              <div className="font-[400] text-base">
-                <i className="fa-solid fa-clock" ></i>
-                <span className="ml-3">2024-04-19 17:00</span>
-              </div>
-              <div className="border-b-[1px] md:hidden z-50"></div>
-            </div>
-          </div>
-        </div>
-        {/* Select Tickets*/ }
-        <div className={`${styles.event} select-ticket lg:w-[50vw] md:w-[38vw]  max-lg:w-[50vw] xl:w-[61vw] max-sm:w-[85vw] md:pr-[100px]`}>
-          <div className="part">
-            <div className="upper-part">Adult </div>
-          </div>
-          <div className="part">
-            <div className="mid-part">
-              <div className="dollar">$50.00</div>
-            </div>
-          </div>
-          <div className=" flex ">
-            <div className="lower-part flex flex-row items-center w-full md:w-auto">
-              <label htmlFor="quantity" className="mr-2">Quantity</label>
-              <TicketDropdown value={selectedTickets} onChange={handleTicketChange} />
-            </div>
-          </div>
-        </div>
-
-
+      {/* Select Tickets*/}
+      {event_seating_type === "general" && is_seats_io === 0 && (
+        <GeneralEvent
+          eventSeatData={eventSeatData}
+          selectedTickets={selectedTickets}
+          handleTicketChange={handleTicketChange}
+          sectionData={sectionData}
+          pricingData={pricingData}
+          handleRemoveTicket={handleRemoveTicket}
+        />
+      )}
+      {event_seating_type === "reserved" && is_seats_io === 1 && (
+        <SeatsIoEvent
+          eventSeatData={eventSeatData}
+          selectedTickets={selectedTickets}
+          handleTicketChange={handleTicketChange}
+          sectionData={sectionData}
+          pricingData={pricingData}
+          handleRemoveTicket={handleRemoveTicket}
+        />
+      )}
+      {event_seating_type === "reserved" && is_seats_io === 0 && (
+        <OldSeatingEvent
+          eventSeatData={eventSeatData}
+          selectedTickets={selectedTickets}
+          handleTicketChange={handleTicketChange}
+          sectionData={sectionData}
+          pricingData={pricingData}
+          handleRemoveTicket={handleRemoveTicket}
+        />
+      )}
     </div>
   );
 };
