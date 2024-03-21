8c1e7c2e91d78e55def1fe9ff02541db4d0ea60e
Manas Khanna <manas.khanna@avisoft.io>
2024-03-21 17:30:00 +0530

    commit

--- src/app/event/index/[eventId]/layout.js
+++ src/app/event/index/[eventId]/layout.js
@@ -1,25 +1,12 @@
-// "use client";
-
 import React from "react";
 
 import Header from "@/components/header/Navbar";
 import Footer from "@/components/footer/footer";
 import { getThemeData, getSingleEventData } from "@/helpers/api-utils";
 import BottomView from "@/components/social-share-widget/BottomView";
-import SocialShareWidget from "@/components/social-share-widget/SocialShare";
-// import { useState } from "react";
 
 export default async function Layout({ children, params }) {
   const { eventId } = params;
-  // const [view, setView] = useState(true);
-
-  // const handleView = () => {
-  //   console.log("Clicked");
-  //   setView(!view);
-  //   console.log("THis is ",view);
-  // };
-
-  let view = true;
 
   const {
     venueName,
@@ -57,8 +44,6 @@ export default async function Layout({ children, params }) {
         sellTicketUrl={sellTicketUrl}
         companyName={companyName}
       />
-      {/* {view && <SocialShareWidget />} */}
-
       <BottomView buttonLinkBoxBorderColor={buttonLinkBoxBorderColor} isEventTypePhysical={isEventTypePhysical} />
     </>
   );
