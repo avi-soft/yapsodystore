1ed061880f2f9b73ebce51f7c215265ad884d5d7
Manas Khanna <manas.khanna@avisoft.io>
2024-03-21 18:50:18 +0530

    commit

--- src/components/social-share-widget/SocialShare.jsx
+++ src/components/social-share-widget/SocialShare.jsx
@@ -5,7 +5,7 @@ import { FaPinterest } from "react-icons/fa";
 import { IoShareSocial } from "react-icons/io5";
 export default function SocialShareWidget() {
   return (
-    <section className="fixed md:bottom-[40%] bottom-[10%] z-20 sm:right-0 max-sm:left-0 block md:rounded bg-[#ffffff] p-[8px] text-[30px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.335)]">
+    <section className="fixed md:bottom-[40%] max-[376px]:bottom-[12%] max-[430px]:bottom-[6%] max-[414px]:bottom-[7%] max-[360px]:bottom-[10%] z-20 sm:right-0 max-sm:left-0 block md:rounded bg-[#ffffff] p-[8px] text-[30px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.335)]">
       <ul>
         <li className="pb-2">
           <Link href={"/"}>
