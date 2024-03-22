22cb1234c476336501b71e95d144b9e8c7f22a52
Manas Khanna <manas.khanna@avisoft.io>
2024-03-22 11:13:27 +0530

    commit

--- src/components/social-share-widget/SocialShare.jsx
+++ src/components/social-share-widget/SocialShare.jsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import { FaFacebookSquare } from "react-icons/fa";
-import { FaTwitterSquare } from "react-icons/fa";
-import Image from "next/image";
+import { FaTwitter } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
 import { IoShareSocial } from "react-icons/io5";
-import Pinterest from "@/../public/pinterest.svg";
 export default function SocialShareWidget() {
   return (
-    <section className="fixed md:bottom-[40%] bottom-[10%] max-[376px]:bottom-[12%] max-[430px]:bottom-[6%] max-[414px]:bottom-[7%] max-[360px]:bottom-[10%] z-20 sm:right-0 max-sm:left-0 block md:rounded bg-[#ffffff] p-[8px] text-[30px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.335)] max-md:bottom-[calc(0+67px)]">
+    <section >
       <ul>
         <li className="pb-2">
           <Link href={"/"}>
@@ -15,14 +14,12 @@ export default function SocialShareWidget() {
         </li>
         <li className="pb-2">
           <Link href={"/"}>
-            <FaTwitterSquare size={24} className="text-[#4099ff]" />
+            <FaTwitter size={24} className="text-[#4099ff]" />
           </Link>
         </li>
         <li className="pb-2">
           <Link href={"/"}>
-
-            <Image src={Pinterest} alt="" height={24} width={23} />
-
+            <FaPinterest size={24} className="text-[#C8232C]" />
           </Link>
         </li>
         <li className="pb-2 hidden md:block">
