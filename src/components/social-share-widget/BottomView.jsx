df57576b1df507b0a4ae03ed328c9128b8bdf661
Manas Khanna <manas.khanna@avisoft.io>
2024-03-22 12:58:56 +0530

    commit

--- src/components/social-share-widget/BottomView.jsx
+++ src/components/social-share-widget/BottomView.jsx
@@ -15,12 +15,12 @@ function BottomView({ buttonLinkBoxBorderColor, isEventTypePhysical }) {
         {" "}
         <SocialShareWidget />
       </div>
-      <div className="hidden max-md:block mt-[57px] max-sm:mt-[65px] max-[375px]:mt-[75px]">
+      <div className="hidden max-md:block mt-[50px] max-sm:mt-[45px] max-[375px]:mt-[45px]">
         <div
-          className={`items-center justify-between text flex z-20 py-[21px] px-[15px] bg-[#e0e1ea] text-white w-[100%] left-0 right-0 border border-white bottom-0 fixed`}
+          className={`max-h-[50px] items-center justify-between text flex z-20 py-[21px] px-[15px] bg-[#e0e1ea] text-white w-[100%] left-0 right-0 border border-white bottom-0 fixed`}
         >
           {view && (
-            <div className="max-md:bottom-[calc(0%+67px)] max-sm:left-0 absolute z-20 right-0 max-sm:right-auto md:rounded bg-[#ffffff] p-[12px] text-[30px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.335)] max-[376px]:bottom-[calc(0%+82px)]">
+            <div className="bottom-[calc(0%+50px)] max-sm:left-0 absolute z-20 right-0 max-sm:right-auto md:rounded bg-[#ffffff] p-[12px] text-[30px] shadow-[0px_1px_4px_0px_rgba(0,0,0,0.335)]">
               <SocialShareWidget />
             </div>
           )}
