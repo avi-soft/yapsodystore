0543afc2074413528e3926416876336d1f48f2d3
Sagar-kamboj-AviSoft <sagar.kamboj@avisoft.io>
2024-03-26 17:08:51 +0530

    commit

--- src/components/box-appear/OnlyForFaq.jsx
+++ src/components/box-appear/OnlyForFaq.jsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { RxCrossCircled } from "react-icons/rx";
-import Script from "next/script";
 
 const ClickFaq = ({ component: Component, onClose }) => {
   const [componentWidth, setComponentWidth] = useState(0);
@@ -12,22 +11,7 @@ const ClickFaq = ({ component: Component, onClose }) => {
     document.getElementById("my_modal_2").showModal();
   }, []);
 
-  // Add event listener for escape key
-  useEffect(() => {
-    const closeOnEscape = (e) => {
-      if (e.key === "Escape") {
-        onClose();
-      }
-    };
-
-    window.addEventListener("keydown", closeOnEscape);
-
-    // Remove event listener on cleanup
-    return () => {
-      window.removeEventListener("keydown", closeOnEscape);
-    };
-  }, [onClose]);
-
+ 
   useEffect(() => {
     function handleResize() {
       const modalContent = document.getElementById("modal-content1");
@@ -42,40 +26,24 @@ const ClickFaq = ({ component: Component, onClose }) => {
     };
   }, []);
 
-  return (
-    <div>
-      {/* <button className="btn" onClick={() => document.getElementById('my_modal_1').showModal()}>open modal</button> */}
-      <Script
-        src="https://kit.fontawesome.com/8d4b434c6d.js"
-        crossorigin="anonymous"
-      />
-      <dialog
-        id="my_modal_2"
-        className="modal"
-        style={{
-          backgroundColor: "rgb(51 48 48 / 84%)",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          className="temporary-button1"
-          style={{ width: componentWidth }}
-          onClick={handleClose}
-        >
-          <div style={{ fontSize: "26px", color: "white" }}>
-            <RxCrossCircled style={{ cursor: "pointer" }} />
-          </div>
-        </div>
+    return (
+        <div>
+            <dialog id="my_modal_2" className="modal" style={{
+                backgroundColor:'rgb(51 48 48 / 84%)',
+            }} onClose={handleClose}>
+                <div className="modal-box" id='modal-content1'>
+                    <Component />
+                </div>
+                <form method="dialog" className="modal-backdrop" onClick={handleClose}>
+                    <button style={{cursor:'auto'}} >close</button>
+                    <div className="temporary-button1" style={{ width: componentWidth }} >
+                        <div ><RxCrossCircled style={{ cursor: 'pointer', width: '24px', height: '24px',color:'white' }} /></div>
+                    </div>
+                </form>
+            </dialog>
 
-        <div className="modal-box" id="modal-content1">
-          <Component />
         </div>
-      </dialog>
-    </div>
-  );
-};
+    )
+}
 
 export default ClickFaq;
