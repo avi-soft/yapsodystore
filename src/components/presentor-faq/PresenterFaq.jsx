0e8c82e568e4f75a3a54cd0c3398f8b4875aae42
Sagar-kamboj-AviSoft <sagar.kamboj@avisoft.io>
2024-03-26 16:22:04 +0530

    commit

--- src/components/presentor-faq/PresenterFaq.jsx
+++ src/components/presentor-faq/PresenterFaq.jsx
@@ -10,14 +10,14 @@ const PresenterFaq = ({ size, color }) => {
   return (
     <div className="font-lato text-[14px] flex">
       <div
-        className="flex cursor-pointer"
+        className="flex items-center cursor-pointer"
         onClick={showFaqfunct}
       >
         <IconLabel
           color={color}
           size={size}
           icon="presenterFaq"
-          gap={3}
+          gap={2}
         ></IconLabel>
         <div className="mt-[2px]" style={{ color: color }}>
           Presenter FAQ
